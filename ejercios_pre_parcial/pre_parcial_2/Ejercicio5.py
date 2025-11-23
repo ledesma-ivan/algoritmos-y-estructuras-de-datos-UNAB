fad276dce7744a5f1367a2d29f295c07e541f04e
ledesma-ivan <ivanarielledesma@gmail.com>
2025-11-23 18:56:51 -0300

    Ejercicio 5

---
+++ ejercios_pre_parcial/pre_parcial_2/Ejercicio5.py
@@ -0,0 +1,14 @@
+'''
+Crear los Objetos: Lobo, Oveja, y Pasto. Cada unos de ellos tendrá un nombre y un método "comer". 
+
+El método comer: 
+
+- "Lobo puede comer oveja"
+
+- "Oveja puede comer pasto"
+
+- También: "Pasto no come a nadie", "Oveja no puede comer Lobo"
+
+
+Pueden implementar cualquier otro método/variables que les sean útiles.     
+'''
