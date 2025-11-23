7f9e7fa1a78b1a5cff32ee9eff6e8785a45e5786
ledesma-ivan <ivanarielledesma@gmail.com>
2025-11-23 19:12:35 -0300

    Ejercicio 1: Completado

--- pre_parciales/pre_parcial_2/ejercicio1.py
+++ pre_parciales/pre_parcial_2/ejercicio1.py
@@ -3,3 +3,23 @@
  y retorne el valor buscado, en caso contrario debe lanzar una excepción. '''
 
 
+def busqueda_binaria(coleccion, valor_buscado):
+    # Convertimos cualquier tipo de estructura de datos a una lista para poder trabajarla.
+    elementos = list(coleccion)
+    
+    inicio = 0
+    fin = len(elementos) - 1
+
+    while inicio <= fin:
+        medio = (inicio + fin) // 2
+        medio_valor = elementos[medio]
+
+        if medio_valor == valor_buscado:
+            return medio_valor
+
+        if medio_valor < valor_buscado:
+            inicio = medio + 1
+        else:
+            fin = medio - 1
+
+    raise ValueError("El valor no se encuentra en la colección")
