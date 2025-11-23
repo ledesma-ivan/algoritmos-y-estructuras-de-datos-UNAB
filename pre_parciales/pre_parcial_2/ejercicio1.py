657df51ccb3bacb1f4ca9488c90d1d9b3922408a
ledesma-ivan <ivanarielledesma@gmail.com>
2025-11-23 18:59:06 -0300

    Cambios de los nombres de las carpetas.

---
+++ pre_parciales/pre_parcial_2/ejercicio1.py
@@ -0,0 +1,5 @@
+'''Escribir un algoritmo de busqueda binaria que reciba como entrada una estructura de datos cualquiera (cualquier tipo de colección de objetos,
+ i.e array, lista, listas enlazadas, etc.) 
+ y retorne el valor buscado, en caso contrario debe lanzar una excepción. '''
+
+
